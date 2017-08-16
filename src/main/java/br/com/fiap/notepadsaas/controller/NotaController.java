f7c426b42ebb8a852e5767b0ab8d5688ced57a2e
Douglas PT <douglaspt@gmail.com>
2017-08-15 21:43:10 -0300

    Primeiro Commit

--- src/main/java/br/com/fiap/notepadsaas/controller/NotaController.java
+++ src/main/java/br/com/fiap/notepadsaas/controller/NotaController.java
@@ -2,15 +2,20 @@ package br.com.fiap.notepadsaas.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.notepadsaas.model.Nota;
 import br.com.fiap.notepadsaas.repository.NotaRepository;
 
 @RestController
+@CrossOrigin
+@RequestMapping(value = "/nota")
 public class NotaController {
 	
 	private NotaRepository notaRepository;
@@ -23,6 +28,11 @@ public class NotaController {
 	public List<Nota> findAll() {
 		return notaRepository.findAll();
 	}
+	
+    @GetMapping(value = "/titulo/{titulo}")
+    private List<Nota> findByTitulo(@PathVariable(value = "titulo") String q) {
+        return notaRepository.findByTitulo(q);
+    }
 	
 	@PostMapping(value = "/")
 	public void save(@RequestBody Nota nota){
